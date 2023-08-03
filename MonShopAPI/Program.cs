16fd61ca2be490b529a45976efc794152ac64a5c
HongQuan <hongquan.contact@gmail.com>
2023-08-03 23:25:50 +0700

    update

--- MonShopAPI/Program.cs
+++ MonShopAPI/Program.cs
@@ -1,23 +1,52 @@
+﻿using Microsoft.OpenApi.Models;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(p => p.AddPolicy(MyAllowSpecificOrigins, builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Yoga Center API",
+        Description = "@Copyright: Hồng Quân",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
-
 app.MapControllers();
+
+// Configure Swagger
+app.UseSwagger(options => {
+    options.SerializeAsV2 = true;
+});
+app.UseSwaggerUI(options => {
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.Run();
---
+++ MonShopLibrary/DAO/CategoryDBContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MonShopLibrary.DTO;
+using MonShopLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonShopLibrary.DAO
+{
+    public class CategoryDBContext: MonShopContext
+    {
+        public CategoryDBContext() { }  
+
+        public async Task<List<Category>> GetAllCategory()
+        {
+            List<Category> list = await this.Categories.ToListAsync();
+            return list;
+        }
+
+        public async Task AddCategory(CategoryDTO dto)
+        {
+            Category category = new Category { CategoryId = dto.CategoryId , CategoryName = dto.CategoryName};
+            await this.Categories.AddAsync(category);
+            await this.SaveChangesAsync();  
+        }
+
+        public async Task UpdateCategory(CategoryDTO dto)
+        {
+            Category category = await this.Categories.FindAsync(dto.CategoryId);
+             category = new Category { CategoryId = dto.CategoryId, CategoryName = dto.CategoryName };
+             this.Categories.Update(category);
+            await this.SaveChangesAsync();
+        }
+
+        public async Task DeleteCategory(CategoryDTO dto)
+        {
+            Category category = await this.Categories.FindAsync(dto.CategoryId);
+            this.Categories.Remove(category);   
+            await this.SaveChangesAsync();  
+        }
+
+    }
+}
