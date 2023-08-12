9cda14d693f12126253174293a5f35a26df5c219
HongQuan <hongquan.contact@gmail.com>
2023-08-12 19:36:54 +0700

    update

--- MonShop.Library/Models/MonShopContext.cs
+++ MonShop.Library/Models/MonShopContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace MonShop.Library.Models
 {
@@ -32,10 +33,14 @@ namespace MonShop.Library.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            IConfiguration config = new ConfigurationBuilder()
+                       .SetBasePath(Directory.GetCurrentDirectory())
+                       .AddJsonFile("appsettings.json", true, true)
+                       .Build();
+            string cs = config["ConnectionStrings:Host"];
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =(local); database = MonShop;uid=sa;pwd=12345;TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer(cs);
             }
         }
 
